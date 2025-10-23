3728f128e424676ed50ef090921bdb3932a48d7e
Himanshi0408 <himanshitiwari680@gmail.com>
2025-10-23 12:12:11 +0530

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { SignupComponent} from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '**', redirectTo: 'login' }
+];
